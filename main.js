d1dc3117d4076e4ce2c6bee48cd156e025a7b9c6
pedo.oliveira88 <pedo.oliveira88@gmail.com>
2025-04-07 01:05:52 +0000

    criação da função para remover a classe button-selectede início do custom-tip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0 
+let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNumber
@@ -15,6 +16,15 @@ function receiveNumberOfPeopleValue(){
 function receiveTipPercentageValue(value){
    tipPercentage = value / 100
 
+   removeClassButtonSelected()
+
    buttonSelected = document.querySelector(`#button-${value}`)
    buttonSelected.classList.add("button-selected")
+}
+
+function removeClassButtonSelected(){
+    if (buttonSelected !==null){
+      buttonSelected.classList.remove("button-selected")
+      buttonSelected = null 
+    }
 }
